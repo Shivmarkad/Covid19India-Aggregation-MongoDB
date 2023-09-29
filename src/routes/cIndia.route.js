2549e109bf3630974c720bf58834a008b3b72ee7
Shivganesh <shivmarkad99@gmail.com>
2023-09-29 15:09:34 +0530

    [Shivganesh] Added: route for getForeignNationalConfirmed api

--- src/routes/cIndia.route.js
+++ src/routes/cIndia.route.js
@@ -3,7 +3,10 @@ import * as cIndiaController from '../controllers/cIndia.controller';
 
 const router = express.Router();
 
-//route to get all users
+//route to get data
 router.get('', cIndiaController.getStateWiseConfimed);
+
+//route to get data
+router.get('/f', cIndiaController.getForeignStateWiseConfimed);
 
 export default router;
