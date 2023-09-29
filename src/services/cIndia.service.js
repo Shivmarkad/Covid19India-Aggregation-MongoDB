a1ec5da2b044d30e37f96f2bf7678f808f3240c9
Shivganesh <shivmarkad99@gmail.com>
2023-09-29 15:12:39 +0530

    [Shivganesh] Added: aggregation pipeline for getForeignStateWiseConfimed api

--- src/services/cIndia.service.js
+++ src/services/cIndia.service.js
@@ -1,6 +1,6 @@
 import CIndia from '../models/CIndia.model';
 
-//get covid data
+//get covid data Indian National
 export const getStateWiseConfimed = async () => {
   const data = await CIndia.aggregate([
     {
@@ -9,9 +9,25 @@ export const getStateWiseConfimed = async () => {
     {
       $group: {
         _id: '$State/UnionTerritory',
-        totalConfirmed: { $sum: '$Confirmed' }
+        totalConfirmed: { $sum: '$ConfirmedIndianNational' }
       }
     }
+  ]);
+  if (data) return data;
+  throw new Error('Details not found');
+};
+
+//get covid data for foreign National
+export const getForeignStateWiseConfimed = async () => {
+  const data = await CIndia.aggregate([
+    { $match: { ConfirmedForeignNational: { $gte: 0 } } },
+    {
+      $group: {
+        _id: '$State/UnionTerritory',
+        totalConfirmed: { $sum: '$ConfirmedForeignNational' }
+      }
+    },
+    { $sort: { total: -1 } }
   ]);
   if (data) return data;
   throw new Error('Details not found');
