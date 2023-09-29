bdd45c118fe9e96404f283c3c03825bd97d0d3f8
Shivganesh <shivmarkad99@gmail.com>
2023-09-29 11:39:17 +0530

    [Shivganesh] Added: Controller for covid19_India  API's

---
+++ src/controllers/cIndia.controller.js
@@ -0,0 +1,24 @@
+import HttpStatus from 'http-status-codes';
+import * as CIndiaService from '../services/cIndia.service';
+
+/**
+ * Controller to get covid19 details available
+ * @param  {object} req - request object
+ * @param {object} res - response object
+ * @param {Function} next
+ */
+export const getData = async (req, res, next) => {
+  try {
+    const data = await CIndiaService.getData();
+    res.status(HttpStatus.OK).json({
+      code: HttpStatus.OK,
+      data: data,
+      message: 'covid19 details fetched successfully'
+    });
+  } catch (error) {
+    res.status(HttpStatus.BAD_REQUEST).json({
+      code: HttpStatus.BAD_REQUEST,
+      message: error.message
+    });
+  }
+};
